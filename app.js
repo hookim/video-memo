c5ecc551b3e8433fdface5554fef56a0c588e75a
HooKim <hooo.dev@gmail.com>
2021-08-11 11:25:18 +0900

    feat: Create local storage aand unique hash id for each memo pages

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-document.querySelector('#create').addEventListener('click', ( ) => {
+let memos = JSON.parse(localStorage.getItem('memos'))     // get memos from local storage
+if (memos === null)  memos = []
+class memo {
+    constructor(hash , title = "Untitled", body = " "){
+        this.title  = title
+        this.body = body
+        this.hash = hash
+    }
+}
+const renderMemo = (memo) => {
     const list = document.createElement('div')
     const link = document.createElement('a')
     
-    link.textContent = "Hello"
-    link.setAttribute('href', '/memo.html')
+    link.textContent = memo.title                        
+    link.setAttribute('href', `/memo.html#${memo.hash}`) 
 
     list.appendChild(link)
     document.querySelector('#display').appendChild(list)
+}
+
+memos.forEach((memo) => {
+    renderMemo(memo)
+})
+
+
+document.querySelector('#create').addEventListener('click', ( ) => {
+    
+    const newMemo = new memo(uuidv4())                      // create new memo object for local storage
+    
+    memos.push(newMemo)                                    // push new memo to the current memo array
+    localStorage.setItem('memos',JSON.stringify(memos))
+
+    renderMemo(newMemo)
+    
+    
 })
