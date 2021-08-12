0dee7904333e8e13fd8512e29e070ac3e4a27c3e
HooKim <hooo.dev@gmail.com>
2021-08-12 12:15:36 +0900

    feat: Create delete-button

--- function.js
+++ function.js
@@ -6,19 +6,37 @@ class memo {
     }
 }
 
-const renderMemo = (memo) => {
-    const list = document.createElement('div')
-    const link = document.createElement('a')
-    
-    link.textContent = memo.title                        
-    link.setAttribute('href', `/memo.html#${memo.hash}`) 
-
-    list.appendChild(link)
-    document.querySelector('#display').appendChild(list)
-}
-
 const getMemos = () => {
     const storedMemos = JSON.parse(localStorage.getItem('memos'))
     if (storedMemos === null) return []
     else return storedMemos
+}
+
+const renderMemo = (memo) => {
+    const display = document.querySelector('#display')
+    const list = document.createElement('div')
+    const link = document.createElement('a')
+    const button = document.createElement('button')
+    
+    link.textContent = memo.title                        
+    link.setAttribute('href', `/memo.html#${memo.hash}`) 
+    button.textContent = 'x'
+
+    list.appendChild(link)
+    list.appendChild(button)
+    display.appendChild(list)
+
+    button.addEventListener('click', () => {            // Button for delete
+        const memos = getMemos()
+        const idx = memos.findIndex((mm) => {
+            return mm.hash === memo.hash
+        })
+        display.removeChild(list)
+        memos.splice(idx,1)
+        localStorage.setItem('memos', JSON.stringify(memos))
+        display.textContent = ''                        // Clear the outdated rendered lists
+        memos.forEach((memo) => {
+            renderMemo(memo)
+        })
+    })
 }
